97a93a1f25fd5665c14b9466ca7c733892fade43
markj1003 <96064065+markj1003@users.noreply.github.com>
2021-12-16 01:55:15 +0000

    Add files via upload
    Working submission

--- client/index.js
+++ client/index.js
@@ -2,33 +2,68 @@ import "./index.scss";
 
 const server = "http://localhost:3042";
 
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
+
+const ec = new EC('secp256k1');
+
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
   if(value === "") {
     document.getElementById("balance").innerHTML = 0;
     return;
   }
 
+  console.log('part 1');
   fetch(`${server}/balance/${value}`).then((response) => {
+    console.log('part2');
     return response.json();
   }).then(({ balance }) => {
+    console.log('part3')
     document.getElementById("balance").innerHTML = balance;
   });
 });
 
+function messageToServer(yourKey, amount, recipient) {
+  const key = ec.keyFromPrivate(yourKey);
+  const message = {
+    key: document.getElementById("exchange-address").value,   // key.getPublic(),
+    amount: amount,
+    recipient: recipient
+  };
+  const msg = JSON.stringify(message);
+  return {message: msg, signature: key.sign(SHA256(msg).toString())};
+}
+
 document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
+  const sender = document.getElementById("private-key").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const msgs = messageToServer(sender, amount, recipient);
 
-  const body = JSON.stringify({
-    sender, amount, recipient
-  });
+  const body = JSON.stringify(msgs);
 
-  const request = new Request(`${server}/send`, { method: 'POST', body });
+  const requesti = new Request(`${server}/send`, { method: 'POST', body });
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
+  fetch(requesti, { headers: { 'Content-Type': 'application/json' }}).then(response => {
     return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
+  }).then(({ balance, outcome }) => {
+    if (balance != -1) {document.getElementById("balance").innerHTML = balance};
+    document.getElementById("outcome").innerHTML = outcome;
   });
+});
+
+document.getElementById("newKey").addEventListener('click', () => {
+  console.log('why');
+  const newKey = ec.genKeyPair();
+  const pubKey = newKey.getPublic();
+  const body = JSON.stringify({pubKey});
+  const request = new Request(`${server}/newKey/`, {method: 'POST', body});
+
+  fetch(request, { headers: {'Content-Type': 'application/json' }}).then(response => {
+      return response.json();
+  }).then(({ming}) => {
+    document.getElementById("private-key").value = newKey.getPrivate().toString(16);
+    document.getElementById("pubkey").innerHTML = "Corresponds to Public Key: " + newKey.getPublic().encode('hex').slice(0, 10);
+  });
+
 });
--- server/index.js
+++ server/index.js
@@ -2,31 +2,85 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const SHA256 = require('crypto-js/sha256');
+
+const EC = require('elliptic').ec;
+
+const ec = new EC('secp256k1');
+
+let initialKeys = 3;
+
+const accounts = {};
+
+for (let key = 0; key < initialKeys; key++) {
+  const testkey = ec.genKeyPair();
+  accounts[testkey.getPublic().encode('hex').slice(0, 10)] = {number: key, pubKey: testkey.getPublic(), balance: 50, private: testkey.getPrivate().toString(16)};
+}
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
-  const balance = balances[address] || 0;
+  let balance;
+  try{balance = accounts[address.toLowerCase()].balance || 0; }
+  catch(err) {balance = 0;}
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {message, signature} = req.body;
+  if (testInput(message, signature)) {
+    const sender = (JSON.parse(message).key);
+    const recipient = JSON.parse(message).recipient.toLowerCase();
+    const amount = JSON.parse(message).amount;
+    
+    if (accounts[recipient] == undefined) {
+      res.send({balance: -1, outcome: "No such account"}); return;}
+    else {
+      if (isNaN(amount)) {res.send({balance: -1, outcome: "Amount to send is not a number!"}); return;}
+      if (accounts[sender].balance < amount) {res.send({balance:-1, outcome: "Balance too Low!"}); return;}
+      accounts[sender].balance -= amount;
+      accounts[recipient].balance = (accounts[recipient].balance || 0) + +amount;
+      res.send({ balance: accounts[sender].balance, outcome: "Money Sent" });
+    }
+  } 
+  else {res.send({balance:-1, outcome: "Incorrect signature, no transaction was completed"});}
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+function testInput(message, signature) {
+  const details = JSON.parse(message);
+  try {details.key = accounts[details.key].pubKey;}
+  catch(err) {return false;}
+  const key = ec.keyFromPublic(details.key);
+  return key.verify(SHA256(message).toString(), signature);  
+}
+
+app.post('/newKey', (req, res) => {
+  const parsed =req.body;
+
+  const pkey = {x: parsed.pubKey[0], y: parsed.pubKey[1]};
+  const key = ec.keyFromPublic(pkey);
+  accounts[key.getPublic().encode('hex').slice(0, 10)] = {number: Object.keys(accounts).length, pubKey: key.getPublic(), balance: 50}
+  res.send({message: 'success'});
 });
+  
+
+
+app.listen(port, () => {
+
+  console.log("Available Accounts:");
+  for (const [account, details] of Object.entries(accounts)) {
+    console.log(details.number, account, details.balance);
+  }
+  console.log("\n");
+  console.log("Private Keys:");
+  for (const [account, details] of Object.entries(accounts)) {
+    console.log(details.number, details.private);
+  }
+});
+
